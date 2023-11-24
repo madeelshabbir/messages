2a9d67fdcf403d040096b0de9b1a30d7bcb2d4f9
Adeel Shabbir <madeelshabbirs@gmail.com>
2023-11-24 22:20:07 +0500

    Implementing Service, Reporsitory and Dependency Injection

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,37 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  NotFoundException,
+} from '@nestjs/common';
 
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+
   @Get()
-  index() {}
+  index() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   create(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  show(@Param('id') id: string) {
-    console.log(id);
+  async show(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('Message not found!');
+    }
+
+    return message;
   }
 }
---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { readFile, writeFile } from 'fs/promises';
+
+@Injectable()
+export class MessagesRepository {
+  async findOne(id: string) {
+    const messages = await this.findAll();
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
+
+  async create(content: string) {
+    const messages = await this.findAll();
+    const id = Date.now().toString();
+    messages[id] = { id, content };
+    await writeFile('messages.json', JSON.stringify(messages));
+    return messages[id];
+  }
+}
